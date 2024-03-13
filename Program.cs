4ec84c70182d61e83d894c6aa177076a741c550b
Christopher-Morrison <72662988+Christopher-Morrison@users.noreply.github.com>
2024-03-13 13:43:01 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace genericAnimalClass
+{
+    public class Animal < T >
+    {
+        public T data;
+        public Animal(T data)
+        {
+            this.data = data;
+            Console.WriteLine("Data passed: "+ this.data);
+        }
+
+        public T getAnimal()
+        {
+            return data;
+        }
+    }
+    class Program{
+        
+        static void Main()
+        {
+           Animal <string> animalName = new Animal <string> ("Poodle");
+           Animal <string> animalHabitat = new Animal<string> ("My Aunt's House");
+           Animal <bool> endangered = new Animal<bool> (false);
+           Animal <bool> extinct = new Animal<bool> (false);
+           Animal <int> averageLifespan = new Animal<int> (13);
+
+           animalName.getAnimal();
+           animalHabitat.getAnimal();
+           endangered.getAnimal();
+           extinct.getAnimal();
+           averageLifespan.getAnimal();
+        }
+    }
+}
